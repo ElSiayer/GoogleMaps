5f7ffa8db47be96c46d75a5c5224b16e2b9e15fa
ElSiayer <60122786+ElSiayer@users.noreply.github.com>
2020-11-15 22:09:15 -0500

    Maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,60 @@
-import { Component } from '@angular/core';
+import { AgmMap } from '@agm/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormGroup, FormControl} from "@angular/forms";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  coordenadas= new FormGroup({
-    latitud: new FormControl(''),
-    longitud:new FormControl('')
-  })
+
+export class AppComponent {  
+  @ViewChild("googleMap") agmMap: AgmMap;
+  direccion= new FormGroup({
+    sectorText: new FormControl(''),
+    ciudadText: new FormControl(''),
+    paisText: new FormControl('')
+  })  
   lista=[{lat: 51.678418,
     lng: 7.809007}]
   google: any;
-  title = 'SemiApi';
-  lat = 51.678418;
-  lng = 7.809007;
+  title = 'Web services';
   
-  recivirCordenadas(){
-    console.log(this.coordenadas.value);    
-    const {latitud, longitud}= this.coordenadas.value
-    this.lista.push({lat: latitud,
-      lng: longitud})
-    console.log(this.lista)
-    console.log(latitud,longitud)    
+  protected map: any;
+  constructor() {}
+  defaultCenter = {lat: 55.5815245, lng: 36.8251383};
+  currentCenter = Object.assign({}, this.defaultCenter);
+  zoom = 3;
+
+  geocode(address: string): Promise<any> {
+    console.log(address);    
+    const geocoder = new google.maps.Geocoder();
+    return new Promise((resolve, reject) => {
+      geocoder.geocode(
+        {
+          address: address
+        },
+        (results, status) => {
+          if (status === google.maps.GeocoderStatus.OK) {
+            resolve(results[0]);
+          } else {
+            reject(new Error(status));
+          }
+        }
+      );
+    });
   }
+  
+findPlace() {
+  const {sectorText,ciudadText,paisText}=this.direccion.value
+  var aux=sectorText+","+ciudadText+","+paisText;
+  this.zoom = 14;
+  this.geocode(aux).then(place => {
+    this.currentCenter = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng()};
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}  
 }
